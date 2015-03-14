17c56f0dd646f448817e0712c5dd6e77caa1bd22
Andrew Mason <mason@case.edu>
2015-03-14 17:11:49 -0400

    adding specs

--- spec/helicopter_spec.rb
+++ spec/helicopter_spec.rb
@@ -5,7 +5,44 @@ describe Helicopter do
     expect(Helicopter::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'defines a subclasses method on anything extending it' do
+    class Foo; extend Helicopter; end
+    class Bar; end
+
+    expect(Foo).to respond_to :subclasses
+    expect(Bar).to_not respond_to :subclasses
+
+    undefine Foo, Bar
+  end
+
+  it 'allows a class to track its descendants' do
+    class Foo; extend Helicopter; end
+    class Bar < Foo; end
+    class Baz < Foo; end
+
+    expect(Foo.subclasses).to eq [Bar, Baz]
+
+    undefine Foo, Bar, Baz
+  end
+
+  it 'tracks only direct descendants' do
+    class Foo; extend Helicopter; end
+    class Bar < Foo; end
+    class Baz < Bar; end
+
+    expect(Foo.subclasses).to eq [Bar]
+
+    undefine Foo, Bar, Baz
+  end
+
+  it "doesn't get confused when multiple things extend it" do
+    class Foo; extend Helicopter; end
+    class Bar; extend Helicopter; end
+    class Baz < Foo; end
+
+    expect(Foo.subclasses).to eq [Baz]
+    expect(Bar.subclasses).to eq []
+
+    undefine Foo, Bar, Baz
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'helicopter'
+
+def undefine(*classes)
+  classes.each do |klass|
+    Object.send(:remove_const, klass.to_s.to_sym)
+  end
+end
